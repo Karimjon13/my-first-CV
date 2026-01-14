30d113edab822b84860b32d9fa9eeaccb8eb5ec5
Karimjon <13babayga@gmail.com>
2026-01-15 00:07:21 +0500

    Types of js

---
+++ 1.js
@@ -0,0 +1,30 @@
+const firstType = 'строчный'
+console.log(firstType ,typeof firstType)
+
+const secondType = 5
+console.log(secondType, typeof secondType)
+
+const thirdType = true
+console.log(thirdType, typeof thirdType)
+
+const fourthType = null
+console.log(fourthType, typeof fourthType)
+
+let fifthType;
+console.log(fifthType, typeof fifthType)
+
+const sixthType ={
+    firstType: 'строчный',
+    secondType: 5,
+    thirdType:true,
+    fourthType: null,
+}
+console.log(sixthType, typeof sixthtype)
+
+
+
+const eighthType = BigInt(10)
+console.log(eighthType, typeof eighthType)
+
+const seventhType = Symbol(seventhType)
+console.log(seventhType, typeof seventhType)
